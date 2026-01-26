381b9a77cfd4c4eb6042479586b8b5d5059fc8b0
CarlosJHG <carjabal@ucm.es>
2026-01-26 20:43:26 +0100

    Completa script para juntar parquets bajo un mismo esquema
    La idea es usar duckdb para analizar el parquet resultante.

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -13,12 +13,13 @@ Falta:
 - Subirlo a una base de datos con la api de polars
 """
 
-
 from __future__ import annotations
+
+from optimize_raw_df import optimize_dataframe, get_sort_column_by_schema
 
 from pathlib import Path
 import polars as pl
-
+import pyarrow.parquet as pq
 
 TARGET_SCHEMA: dict[str, pl.DataType] = {
     "VendorID": pl.String, # yellow, green
@@ -27,10 +28,10 @@ TARGET_SCHEMA: dict[str, pl.DataType] = {
     "passenger_count": pl.Int32, # yellow, green
     "trip_distance": pl.Float64, # yellow, uber: trip_miles, green
     "RatecodeID": pl.Int32, # yellow, green
-    "store_and_fwd_flag": pl.String, # yellow, green
+    "store_and_fwd_flag": pl.Boolean, # yellow, green
     "PULocationID": pl.Int32, # yellow, uber, green
     "DOLocationID": pl.Int32, # yellow, uber, green
-    "payment_type": pl.Int32, # yellow, green
+    "payment_type": pl.String, # yellow, green
     "fare_amount": pl.Float64, # yellow, uber: base_passenger_fare, green
     "extra": pl.Float64, # yellow, green
     "mta_tax": pl.Float64, # yellow, green
@@ -40,8 +41,7 @@ TARGET_SCHEMA: dict[str, pl.DataType] = {
     "total_amount": pl.Float64, # yellow, green, uber: base_passenger_fare + tolls + bcf + sales_tax + congestion_surcharge + airport_fee + cbd_congestion_fee
     "congestion_surcharge": pl.Float64, # yellow, uber, green
     "airport_fee": pl.Float64, # yellow: Airport_fee, uber
-    "cbd_congestion_fee": pl.Float64, # yellow, uber
-    "hvfhs_license_num": pl.String, # uber
+    "cbd_congestion_fee": pl.Float64, # yellow, uber, green
     "dispatching_base_num": pl.String, # uber
     "originating_base_num": pl.String, # uber
     "request_datetime": pl.Datetime("us"), # uber
@@ -49,198 +49,218 @@ TARGET_SCHEMA: dict[str, pl.DataType] = {
     "bcf": pl.Float64, # uber
     "sales_tax": pl.Float64, # uber
     "driver_pay": pl.Float64, # uber
-    "shared_request_flag": pl.String, # uber
-    "shared_match_flag": pl.String,# uber
-    "access_a_ride_flag": pl.String,# uber
-    "wav_request_flag": pl.String,# uber
-    "wav_match_flag": pl.String,# uber
-    "trip_type":pl.Int32 # green
+    "shared_request_flag": pl.Boolean, # uber
+    "shared_match_flag": pl.Boolean,# uber
+    "access_a_ride_flag": pl.Boolean,# uber
+    "wav_request_flag": pl.Boolean,# uber
+    "wav_match_flag": pl.Boolean,# uber
+    "trip_type": pl.String, # green
+    "ehail_fee": pl.Float64, # green
+    "hvfhs_license_num": pl.String, # uber USAR
 
     # trip_time: uber
+    # black_car_fund #uber
 }
 
-def _find_parquets(root_dir: str, filename: str) -> list[str]:
-    root = Path(root_dir)
-    return sorted(p.as_posix() for p in root.rglob(filename))
 
-def _normalize_to_target_schema(lf: pl.LazyFrame) -> pl.LazyFrame:
-    # Asegura que existan todas las columnas del esquema (las que falten -> null)
+# ---------- 1) Yellow ----------
+yellow_params = {
+    'in_parquet_names': "yellow_taxi.parquet",
+
+    'coalesce': [("Airport_fee", "airport_fee")],
+
+    'rename' : {
+            "tpep_pickup_datetime": "pickup_datetime",
+            "tpep_dropoff_datetime": "dropoff_datetime"
+    },
+
+    'required_schema' : ["VendorID", "pickup_datetime", "dropoff_datetime", "passenger_count", "trip_distance",
+        "RatecodeID", "store_and_fwd_flag", "PULocationID", "DOLocationID", "payment_type", "fare_amount", "extra",      
+        "mta_tax", "tip_amount", "tolls_amount", "improvement_surcharge", "total_amount", "congestion_surcharge",
+        "airport_fee", "cbd_congestion_fee"]
+}
+
+# ---------- 2) Green ----------
+green_params = {
+    'in_parquet_names': "green_taxi.parquet",
+    'rename': {
+        "lpep_pickup_datetime": "pickup_datetime",
+        "lpep_dropoff_datetime": "dropoff_datetime",
+    },
+
+    'required_schema': ["pickup_datetime", "dropoff_datetime", "passenger_count", "trip_distance", "store_and_fwd_flag",
+        "PULocationID", "DOLocationID", "RatecodeID", "payment_type", "fare_amount", "extra", "mta_tax", "tip_amount",
+        "tolls_amount", "improvement_surcharge", "total_amount", "congestion_surcharge", "cbd_congestion_fee",
+        "trip_type", "VendorID", "ehail_fee"]
+}
+    
+# ---------- 3) Uber / HVFHV ----------
+uber_params = {
+    'in_parquet_names': "uber.parquet",
+
+    'rename': {
+        "trip_miles": "trip_distance",
+        "base_passenger_fare": "fare_amount",
+        "tolls": "tolls_amount",
+        "tips": "tip_amount",
+    },
+
+    'required_schema': ["hvfhs_license_num","dispatching_base_num","originating_base_num","request_datetime","on_scene_datetime",
+        "pickup_datetime","dropoff_datetime","PULocationID","DOLocationID","trip_distance","fare_amount","tolls_amount","tip_amount",
+        "driver_pay","bcf","sales_tax","congestion_surcharge","airport_fee","cbd_congestion_fee","shared_request_flag",
+        "shared_match_flag","access_a_ride_flag","wav_request_flag","wav_match_flag", "total_amount"]
+}
+
+
+def _yn10_to_bool(expr: pl.Expr) -> pl.Expr:
+    s = expr.cast(pl.Utf8, strict=False).str.strip_chars().str.to_uppercase()
+    mapping = {
+        "Y": True, "1": True, "1.0": True,
+        "N": False, "0": False, "0.0": False,
+    }
+    return s.replace_strict(mapping, default=None)
+
+def _normalize_payment_type_label(expr: pl.Expr) -> pl.Expr:
+    s2 = (
+        expr.cast(pl.Utf8, strict=False)
+            .str.strip_chars()
+            .str.replace_all(",", "")
+            .str.to_lowercase()
+    )
+
+    mapping = {
+        # numérico
+        "0": "flex_fare_tip",
+        "1": "credit_card",
+        "2": "cash",
+        "3": "no_charge",
+        "4": "dispute",
+        "5": "unknown",
+        "6": "voided",
+        # sinónimos texto
+        "credit": "credit_card", "cre": "credit_card", "crd": "credit_card",
+        "cash": "cash", "cas": "cash", "csh": "cash",
+        "no charge": "no_charge", "no": "no_charge",
+        "dispute": "dispute", "dis": "dispute",
+    }
+    return s2.replace_strict(mapping, default=None)
+
+def _normalize_trip_type_label(expr: pl.Expr) -> pl.Expr:
+    s = expr.cast(pl.Utf8, strict=False).str.strip_chars()
+    mapping = {"1": "street_hail", "2": "dispatch"}
+    return s.replace_strict(mapping, default=None)
+
+FIELD_TRANFORMATIONS: dict[str, TransformationFunc] = {
+    "payment_type": _normalize_payment_type_label(pl.col("payment_type")).alias("payment_type"),
+    "trip_type": _normalize_trip_type_label(pl.col("trip_type")).alias("trip_type"),
+    "hvfhs_license_num": pl.col("hvfhs_license_num").replace({'HV0002':'Juno', 'HV0003':'Uber', 'HV0004':'Via', 'HV0005':'Lyft'}).alias("hvfhs_license_num")
+}
+
+# estos campos se crean al final de las transformaciones en caso de que la columna no exista
+FIELD_DERIVATIONS: dict[str, TransformationFunc] = {
+    "total_amount": (
+                        pl.coalesce([pl.col("fare_amount"), pl.lit(0.0)]) +
+                        pl.coalesce([pl.col("tolls_amount"), pl.lit(0.0)]) +
+                        pl.coalesce([pl.col("bcf"), pl.lit(0.0)]) +
+                        pl.coalesce([pl.col("sales_tax"), pl.lit(0.0)]) +
+                        pl.coalesce([pl.col("congestion_surcharge"), pl.lit(0.0)]) +
+                        pl.coalesce([pl.col("airport_fee"), pl.lit(0.0)]) +
+                        pl.coalesce([pl.col("cbd_congestion_fee"), pl.lit(0.0)])
+                    ).alias("total_amount")
+}
+
+
+def _normalize_to_target_schema(lf: pl.LazyFrame, coalesce: list = [], rename:dict = {}, required_schema:list = []) -> pl.LazyFrame:
+    existing = set(lf.collect_schema().names())
+
+    # asegurar que tiene las columnas de coalesce
+    needed = {c for pair in coalesce for c in pair}  # set de l y r
+    missing = [c for c in needed if c not in existing]
+
     lf = lf.with_columns([
-        pl.lit(None).alias(col)
-        for col in TARGET_SCHEMA.keys()
-        if col not in lf.collect_schema()
-    ])  # with_columns reemplaza/añade columnas [web:518]
-
-    # Castea y ordena columnas; strict=False => si no castea, null [web:520]
-    return lf.select([
-        pl.col(col).cast(dtype, strict=False).alias(col)
-        for col, dtype in TARGET_SCHEMA.items()
+        pl.lit(None).alias(c)
+        for c in missing
     ])
 
+    # las columnas primeras del coalesce se eliminan
+    drop = {l for l, _ in coalesce}
 
-def _scan_many(paths: list[str], hive_partitioning: bool = False) -> pl.LazyFrame:
+    lf = (
+        lf.rename(rename, strict=False)
+        .with_columns([pl.coalesce(l, r).alias(r) for l, r in coalesce])
+        .drop(drop, strict=False)
+    )
+
+    # actualizar las columnas existentes
+    existing = (
+        (((existing | {r for _, r in coalesce}) - drop) - set(rename.keys()))
+        | set(rename.values())
+    )
+
+    # rellenar las columnas que faltan en el esquema con None
+    lf = lf.with_columns([pl.lit(None).alias(col) for col in required_schema if col not in existing])
+
+    # castea tipos o hacer tranformaciones
+    casteos = []
+    for col in required_schema:
+        tipo = TARGET_SCHEMA[col]
+        if tipo == pl.Boolean:
+            casteos.append(_yn10_to_bool(pl.col(col)).alias(col))
+        elif col in FIELD_TRANFORMATIONS:
+            casteos.append(FIELD_TRANFORMATIONS[col])
+        else:
+            casteos.append(pl.col(col).cast(tipo, strict=False).alias(col))
+    
+    lf = lf.select(casteos)
+
+
+    # finalmente, derivar las columnas necesarias
+    return lf.with_columns([deriv for col, deriv in FIELD_DERIVATIONS.items() if col in required_schema and col not in existing])
+
+
+def _find_parquets(root: Path, filename: str) -> list[str]:
+    return sorted(p for p in root.rglob(filename))
+
+def _scan_many(paths: list[Path], hive_partitioning: bool = False) -> pl.LazyFrame:
     if not paths:
         raise FileNotFoundError("No se encontraron parquets.")
     lfs = [pl.scan_parquet(p, hive_partitioning=hive_partitioning) for p in paths]
     return pl.concat(lfs, how="diagonal_relaxed")
 
+def _append_lf_to_single_parquet(lf: pl.LazyFrame, writer: pq.ParquetWriter | None, out_file: Path):
+    df = lf.collect()
+    table = df.to_arrow()
+
+    if writer is None:
+        writer = pq.ParquetWriter(out_file, table.schema, compression="zstd")
+
+    writer.write_table(table)
+    return writer
 
 
-# ---------- 1) Yellow ----------
-def load_yellow(root_dir: str, hive_partitioning: bool = False) -> pl.LazyFrame:
-    paths = _find_parquets(root_dir, "yellow_taxi.parquet")
-    lf = _scan_many(paths, hive_partitioning=hive_partitioning)
+def process_parquets(root_path: Path|str, out_file: Path|str, in_parquet_names: str, sim_read: int = 4, 
+                    coalesce: list = [], rename:dict = {}, required_schema:list = []):
 
-    # Mapeo de columnas originales -> unificadas
-    lf = lf.rename({
-        "tpep_pickup_datetime": "pickup_datetime",
-        "tpep_dropoff_datetime": "dropoff_datetime",
-        "passenger_count": "passenger_count",
-        "trip_distance": "trip_distance",
-        "store_and_fwd_flag": "store_and_fwd_flag",
-        "PULocationID": "PULocationID",
-        "DOLocationID": "DOLocationID",
-        "RatecodeID": "RatecodeID",
-        "payment_type": "payment_type",
-        "fare_amount": "fare_amount",
-        "extra": "extra",
-        "mta_tax": "mta_tax",
-        "tip_amount": "tip_amount",
-        "tolls_amount": "tolls_amount",
-        "improvement_surcharge": "improvement_surcharge",
-        "total_amount": "total_amount",
-        "congestion_surcharge": "congestion_surcharge",
-        "airport_fee": "airport_fee",
-        "cbd_congestion_fee": "cbd_congestion_fee",
-        "VendorID": "VendorID",
-    })
+    root_path = Path(root_path)
+    out_file = Path(out_file)
+    out_file.parent.mkdir(parents=True, exist_ok=True)
 
-    lf = lf.with_columns(
-        pl.lit("Yellow Taxi").alias("hvfhs_license_num")   # indica tipo de transporte
-    )
+    writer: pq.ParquetWriter | None = None
+    parquets_dirs = _find_parquets(root_path, in_parquet_names)
+    try:
+        for i in range(0, len(parquets_dirs), sim_read):
+            proc_path = parquets_dirs[i:i+sim_read]
+            lf = _scan_many(proc_path)
+            lf = _normalize_to_target_schema(lf, coalesce=coalesce, rename=rename, required_schema=required_schema)
+            writer = _append_lf_to_single_parquet(lf, writer, out_file)
 
-    return _normalize_to_target_schema(lf)
-
-
-# ---------- 2) Green ----------
-def load_green(root_dir: str, hive_partitioning: bool = False) -> pl.LazyFrame:
-    paths = _find_parquets(root_dir, "green_taxi.parquet")
-    lf = _scan_many(paths, hive_partitioning=hive_partitioning)
-
-    lf = lf.rename({
-        "lpep_pickup_datetime": "pickup_datetime",
-        "lpep_dropoff_datetime": "dropoff_datetime",
-        "passenger_count": "passenger_count",
-        "trip_distance": "trip_distance",
-        "store_and_fwd_flag": "store_and_fwd_flag",
-        "PULocationID": "PULocationID",
-        "DOLocationID": "DOLocationID",
-        "RatecodeID": "RatecodeID",
-        "payment_type": "payment_type",
-        "fare_amount": "fare_amount",
-        "extra": "extra",
-        "mta_tax": "mta_tax",
-        "tip_amount": "tip_amount",
-        "tolls_amount": "tolls_amount",
-        "improvement_surcharge": "improvement_surcharge",
-        "total_amount": "total_amount",
-        "congestion_surcharge": "congestion_surcharge",
-        "cbd_congestion_fee": "cbd_congestion_fee",
-        "trip_type": "trip_type",
-        "VendorID": "VendorID",
-    })
-
-    
-    lf = lf.with_columns(
-        pl.lit("Green Taxi").alias("hvfhs_license_num")   # indica tipo de transporte
-    )
-
-    return _normalize_to_target_schema(lf)
-
-
-# ---------- 3) Uber / HVFHV ----------
-def load_uber(root_dir: str, hive_partitioning: bool = False) -> pl.LazyFrame:
-    paths = _find_parquets(root_dir, "uber.parquet")
-    lf = _scan_many(paths, hive_partitioning=hive_partitioning)
-
-    # Renombres a tu esquema unificado
-    lf = lf.rename({
-        "hvfhs_license_num": "hvfhs_license_num",
-        "dispatching_base_num": "dispatching_base_num",
-        "originating_base_num": "originating_base_num",
-        "request_datetime": "request_datetime",
-        "on_scene_datetime": "on_scene_datetime",
-        "pickup_datetime": "pickup_datetime",
-        "dropoff_datetime": "dropoff_datetime",
-        "PULocationID": "PULocationID",
-        "DOLocationID": "DOLocationID",
-        "trip_miles": "trip_distance",
-        "base_passenger_fare": "fare_amount",
-        "tolls": "tolls_amount",
-        "tips": "tip_amount",
-        "driver_pay": "driver_pay",
-        "bcf": "bcf",
-        "sales_tax": "sales_tax",
-        "congestion_surcharge": "congestion_surcharge",
-        "airport_fee": "airport_fee",
-        "cbd_congestion_fee": "cbd_congestion_fee",
-        "shared_request_flag": "shared_request_flag",
-        "shared_match_flag": "shared_match_flag",
-        "access_a_ride_flag": "access_a_ride_flag",
-        "wav_request_flag": "wav_request_flag",
-        "wav_match_flag": "wav_match_flag",
-    })
-
-    # Asegura que los sumandos existan (si faltan, null -> luego coalesce a 0.0)
-    lf = lf.with_columns([
-        pl.lit(None).alias(c)
-        for c in [
-            "fare_amount", "tolls_amount", "bcf", "sales_tax",
-            "congestion_surcharge", "airport_fee", "tip_amount", "cbd_congestion_fee"
-        ]
-        if c not in lf.collect_schema()
-    ])
-
-    # Calcula total_amount = base_passenger_fare + tolls + bcf + sales_tax + congestion_surcharge + airport_fee + tips + cbd_congestion_fee
-    # coalesce(null, 0) para que no se vuelva null si falta algún componente
-    lf = lf.with_columns([
-        (
-            pl.coalesce([pl.col("fare_amount"), pl.lit(0.0)]) +
-            pl.coalesce([pl.col("tolls_amount"), pl.lit(0.0)]) +
-            pl.coalesce([pl.col("bcf"), pl.lit(0.0)]) +
-            pl.coalesce([pl.col("sales_tax"), pl.lit(0.0)]) +
-            pl.coalesce([pl.col("congestion_surcharge"), pl.lit(0.0)]) +
-            pl.coalesce([pl.col("airport_fee"), pl.lit(0.0)]) +
-            pl.coalesce([pl.col("cbd_congestion_fee"), pl.lit(0.0)])
-        ).alias("total_amount")
-    ])
-
-    return _normalize_to_target_schema(lf)
-
-    # start_py = datetime(year, month, 1)
-    # end_py = start_py + relativedelta(months=1)
-    # start = pl.lit(start_py)
-    # end = pl.lit(end_py)
-
-    # url = url_raw.format(year=year, month=month)
-
-    # lf = (
-    #     pl.scan_parquet(url).filter(
-    #         (pl.col("tpep_pickup_datetime") >= start) &
-    #         (pl.col("tpep_pickup_datetime") < end) &
-    #         (pl.col("tpep_dropoff_datetime") >= pl.col("tpep_pickup_datetime")) &
-    #         (pl.col("tpep_dropoff_datetime") - pl.col("tpep_pickup_datetime") <= pl.duration(hours=24)) &
-    #         (pl.col("trip_distance") > 0)
-    #     )
+    finally:
+        if writer is not None:
+            writer.close()
 
 if __name__ == "__main__":
-    lf_y = load_yellow("data", hive_partitioning=True)
-    lf_g = load_green("data", hive_partitioning=True)
-    lf_u = load_uber("data", hive_partitioning=True)
+    out_dir = Path.cwd() / "data"
 
-    # Si quieres unificar todo en uno:
-    lf_all = pl.concat([lf_y, lf_g, lf_u], how="vertical_relaxed")
-
-    lf_all.sink_parquet(Path.cwd() / "data" / "trayectos.parquet")
-
-    
+    process_parquets(out_dir, out_dir / "all_yellow_taxi.parquet", **yellow_params)
+    process_parquets(out_dir, out_dir / "all_green_taxi.parquet", **green_params)
+    process_parquets(out_dir, out_dir / "all_uber.parquet", sim_read=1, **uber_params)
