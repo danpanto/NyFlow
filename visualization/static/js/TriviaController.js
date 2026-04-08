70f3b712ccf31d14f6b3a2b0d01003200590d4c1
CarlosJHG <carjabal@ucm.es>
2026-04-08 10:58:22 +0200

    Arregla el modo oscuro en el juego

--- visualization/static/js/TriviaController.js
+++ visualization/static/js/TriviaController.js
@@ -2,6 +2,7 @@ import { ZoneController } from "./ZoneController.js";
 import { getDistanceColors } from "./color.js";
 import { zoneData } from "./services/ZoneDataService.js";
 import { filterService } from "./services/FilterService.js";
+import { themeService } from "./services/ThemeService.js";
 
 export class TriviaController extends ZoneController {
     constructor(backend) {
@@ -11,6 +12,10 @@ export class TriviaController extends ZoneController {
         this.normalOpacity = 0.1;
         this.hoverOpacity = 0.4;
         this.selectOpacity = 0.8;
+
+        this.normalOpacityDark = 0.3;
+        this.hoverOpacityDark = 0.6;
+        this.selectOpacityDark = 1;
 
         this.attempts = 0;
         this.targetId = null;
@@ -70,45 +75,76 @@ export class TriviaController extends ZoneController {
     }
 
     getStyle(id) {
-        if(!this.zoneInfo) return { opacity: 0, fillOpacity: 0 };
+        if(!this.zoneInfo || !themeService) return { opacity: 0, fillOpacity: 0 };
 
         const isSelected = filterService.isSelectedZone(id);
-        const opacity = isSelected ? this.selectOpacity : this.normalOpacity;
-
+        const isLight = themeService.theme === 'light';
         const {color, isFound} = this.zoneInfo[id];
-        return {
-            color: isFound? "grey":"black",
-            fillColor: isFound? color:"indigo",
-            fillOpacity: isFound? 1:opacity,
-            weight: 1,
-        };
+
+        if (isLight){
+            const opacity = isSelected ? this.selectOpacity : this.normalOpacity;
+
+            return {
+                color: isFound? "grey":"black",
+                fillColor: isFound? color:"indigo",
+                fillOpacity: isFound? 1:opacity,
+                weight: 1,
+            };
+        }else{
+            const opacity = isSelected ? this.selectOpacityDark : this.normalOpacityDark;
+
+            return {
+                color: isFound? "grey":"white",
+                fillColor: isFound? color:"blue",
+                fillOpacity: isFound? 1:opacity,
+                weight: 1,
+            };
+        }
+        
     }
 
     onHover(e, id) {
-        if(!this.zoneInfo) return { opacity: 0, fillOpacity: 0 };
+        if(!this.zoneInfo || !themeService) return { opacity: 0, fillOpacity: 0 };
 
         const target = e.target;
         const isSelected = filterService.isSelectedZone(id);
         if(isSelected) return;
-        
+
+        const isLight = themeService.theme === 'light';
         const {isFound} = this.zoneInfo[id];
-        target.setStyle({
-            fillOpacity: isFound? 1:this.hoverOpacity,
-        });
+
+        if (isLight){
+            target.setStyle({
+                fillOpacity: isFound? 1:this.hoverOpacity,
+            });
+        }else{
+            target.setStyle({
+                fillOpacity: isFound? 1:this.hoverOpacityDark,
+            });
+        }  
     }
 
     onUnhover(e, id) {
-        if(!this.zoneInfo) return { opacity: 0, fillOpacity: 0 };
+        if(!this.zoneInfo || !themeService) return { opacity: 0, fillOpacity: 0 };
 
         const target = e.target;
         const isSelected = filterService.isSelectedZone(id);
 
         if(isSelected) return;
 
+        const isLight = themeService.theme === 'light';
         const {isFound} = this.zoneInfo[id];
-        target.setStyle({
-            fillOpacity: isFound? 1:this.normalOpacity,
-        });
+
+        if (isLight){
+            target.setStyle({
+                fillOpacity: isFound? 1:this.normalOpacity,
+            });
+        }else{
+            target.setStyle({
+                fillOpacity: isFound? 1:this.normalOpacityDark,
+            });
+        }
+
     }
 
     onClick(e, id) {
